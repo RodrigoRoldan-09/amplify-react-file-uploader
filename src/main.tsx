180d91d8b7f88f64fc4b822228df9e48396b9141
RodrigoRoldan-09 <rodrigoem.roldan@gmail.com>
2025-08-06 22:55:20 -0600

    Fix JSON module declaration

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+// src/main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Amplify } from 'aws-amplify'
+import App from './App.tsx'
+import './index.css'
+import outputs from './amplify_outputs.json'
 
-Amplify.configure(outputs);
+Amplify.configure(outputs)
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
